77a1ed999f98bb5220cac4bd0008d12c38abc612
Abhiram Reddy  Duggempudi <40287643+AbhiramReddyDuggempdi@users.noreply.github.com>
2018-12-23 17:24:16 +0530

    login

---
+++ logbot.py
@@ -0,0 +1,29 @@
+from selenium import webdriver
+#pip install selenium
+import time
+#download chrome web driver and add it to path
+options = webdriver.ChromeOptions()
+options.add_argument('--ignore-certificate-errors')
+options.add_argument("--test-type")
+options.binary_location = "/usr/bin/chromium"
+driver = webdriver.Chrome()
+# I am using chrome for this
+driver.get('https://sitetobeloggedin')
+#opens the site in a new browser window
+#this time mentioned is for the user to see
+text_area = driver.find_element_by_id('username')
+#finding where to fill the text
+#sendig the text to be filled
+text_area.send_keys("username")
+text_area = driver.find_element_by_id('password')
+text_area.send_keys("typepassword")
+# click submit button
+submit_button = driver.find_elements_by_xpath('//*[@id="app__container"]/div[1]/div/form/div[3]/button')[0]
+#chrome inspect and copy the x path of the button to be clicked
+submit_button.click()
+time.sleep(6)
+#waiting time
+driver.quit()
+
+#finally closes it
+
